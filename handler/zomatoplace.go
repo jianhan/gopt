da171168fe97babfcce14fc3e7dfd5045d7f6a47
Jian Han <jianhan@mail.com>
2018-09-08 20:43:25 +1000

    [refactor] Update zomato handler

--- handler/zomatoplace.go
+++ handler/zomatoplace.go
@@ -6,29 +6,29 @@ import (
 	"net/http"
 )
 
-type zomatoPlace struct {
+type zomato struct {
 	cache *bigcache.BigCache
 }
 
 func NewZomatoPlace(cache *bigcache.BigCache) APIRouter {
-	return &zomatoPlace{cache: cache}
+	return &zomato{cache: cache}
 }
 
-func (p *zomatoPlace) SetupSubrouter(parentRouter *mux.Router) {
+func (z *zomato) SetupSubrouter(parentRouter *mux.Router) {
 	r := parentRouter.PathPrefix("/zomato").Subrouter().StrictSlash(true)
-	r.HandleFunc("/search", p.search).Name("get.zomato.search").Methods("GET")
-	r.HandleFunc("/collections", p.collections).Name("get.zomato.collections").Methods("GET")
-	r.HandleFunc("/categories", p.categories).Name("get.zomato.categories").Methods("GET")
+	r.HandleFunc("/search", z.search).Name("get.zomato.search").Methods("GET")
+	r.HandleFunc("/collections", z.collections).Name("get.zomato.collections").Methods("GET")
+	r.HandleFunc("/categories", z.categories).Name("get.zomato.categories").Methods("GET")
 }
 
-func (p *zomatoPlace) search(rsp http.ResponseWriter, req *http.Request) {
-
-}
-
-func (p *zomatoPlace) collections(rsp http.ResponseWriter, req *http.Request) {
+func (z *zomato) search(rsp http.ResponseWriter, req *http.Request) {
 
 }
 
-func (p *zomatoPlace) categories(rsp http.ResponseWriter, req *http.Request) {
+func (z *zomato) collections(rsp http.ResponseWriter, req *http.Request) {
+
+}
+
+func (z *zomato) categories(rsp http.ResponseWriter, req *http.Request) {
 
 }
